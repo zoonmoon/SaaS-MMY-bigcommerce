71df5504c42fd40cd5f45421930124e76991f07c
Arjun Poudel <arjunpoudel479@gmail.com>
2025-05-04 15:35:57 +0545

    ..

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ if (!storeHash) {
 
 export default defineConfig({
   define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.NODE_ENV': JSON.stringify('production'),
     'process.env.NEXT_PUBLIC_STORE_HASH': JSON.stringify(storeHash),
     'process.env.NEXT_PUBLIC_API_URL': JSON.stringify(process.env.NEXT_PUBLIC_API_URL), // use production url
   },

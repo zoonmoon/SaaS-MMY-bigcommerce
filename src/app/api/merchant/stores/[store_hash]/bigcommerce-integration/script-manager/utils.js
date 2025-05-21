2779836a78080d89b7d4e39e29300761b7be2c4e
Arjun Poudel <arjunpoudel479@gmail.com>
2025-05-21 20:52:11 +0545

    ..

--- src/app/api/merchant/stores/[store_hash]/bigcommerce-integration/script-manager/utils.js
+++ src/app/api/merchant/stores/[store_hash]/bigcommerce-integration/script-manager/utils.js
@@ -7,13 +7,15 @@ const requiredScripts = ["Header YMM Script", "Main YMM Script"];
 export async function createRequiredScriptsInBigCommerce(store_hash, access_token, baseDomain, scriptsToCreateArray = requiredScripts) {
     const scriptAPIEndpoint = `https://api.bigcommerce.com/stores/${store_hash}/v3/content/scripts`;
 
+    const publicDir = path.join(process.cwd(), 'public', 'ymm-scripts');
+
+    const preRequirementPath = path.join(publicDir, 'pre_requirement.html');
+
+
+
     const storeScriptURL = `${baseDomain}/ymm-scripts/main-v1.js`;
 
     const createdScripts = [];
-    
-    // Ensure folder exists before copying
-    await fs.mkdir(storeScriptFolder, { recursive: true });
-
 
     try {   
         for (const scriptName of scriptsToCreateArray) {
